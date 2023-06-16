16c5cf2d5e00ce04c1eb33fd68abc628b97a87c7
felix <1393491999@qq.com>
2023-06-16 17:16:08 +0800

    refactor: 优化路由组件

--- src/router/browser-router.tsx
+++ src/router/browser-router.tsx
@@ -1,5 +1,5 @@
 // 封装 BrowserRouter 处理组件外使用路由跳转页面不更新的问题
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import type { History } from 'history';
 import type { BrowserRouterProps as NativeBrowserRouterProps } from 'react-router-dom';
 import { Router } from 'react-router-dom';
@@ -10,12 +10,18 @@ export interface BrowserRouterProps extends Omit<NativeBrowserRouterProps, 'wind
 
 const BrowserRouter: React.FC<BrowserRouterProps> = React.memo((props) => {
   const { history, ...restProps } = props;
-  const [state, setState] = React.useState({
+  console.log('000 BrowserRouter history', history);
+  const [state, setState] = useState({
     action: history.action,
-    location: history.location,
+    location: history.location
   });
 
-  React.useLayoutEffect(() => history.listen(setState), [history]);
+  useLayoutEffect(() => {
+    // 监听到 history 路由改变时，重新设置当前的 action 和 location
+    // 调用setState后会触发组件重新渲染，从history对象中重新初始化最新的action 和 location
+    // 在其他地方调用 history.push 等会触发 history.listen
+    history.listen(setState);
+  }, []);
 
   return <Router {...restProps} location={state.location} navigationType={state.action} navigator={history}/>;
 });
