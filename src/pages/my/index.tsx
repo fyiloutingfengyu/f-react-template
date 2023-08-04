d96d984f17e3b0ecb5b7c7576addd29823060ea0
felix <1393491999@qq.com>
2023-08-04 10:39:02 +0800

    refactor: 优化公共请求

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'antd-mobile';
 import './index.scss';
-import { http } from '@/utils/http';
+import http from '@/utils/http';
 import { loginApi } from '@/api/login';
 import history from '@/router/history';
 
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,6 +4,7 @@
 import axios from 'axios';
 import { Toast, ToastShowProps } from 'antd-mobile';
 import { HttpOptions } from '@/interface/common';
+import { UrlArr, HeadersObj, AxiosConfig } from '@/interface/http';
 import { removeLocalStorage, getLocalStorage } from './common';
 import { ignoreTokenUrl } from '@/api/ignore-token';
 import { STORAGE_NAME } from './constant';
@@ -11,13 +12,10 @@ import { API_BASE_URL } from '@/config';
 import history from '../router/history';
 import { showFailToast } from './package-antd-mobile';
 
-axios.defaults.baseURL = API_BASE_URL;
-// 请求超时时间
-axios.defaults.timeout = 30000;
-
-interface UrlArr {
-  [key: string]: any;
-}
+const request = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 30000
+});
 
 const urlArr: UrlArr = {};
 let count = 0;
@@ -85,11 +83,6 @@ const http = (options: HttpOptions) => {
   // todo f
   const accessToken = getLocalStorage(STORAGE_NAME.TOKEN);
 
-  interface HeadersObj {
-    'Content-Type': string;
-    Authorization?: string;
-  }
-
   const headersObj: HeadersObj = {
     'Content-Type': getContentType(options.type),
     Authorization: ''
@@ -112,16 +105,67 @@ const http = (options: HttpOptions) => {
     }
   }
 
-  axios.interceptors.request.use(
+  // 请求不能重复
+  if (!options.isRepeatable) {
+    const requestUrl = options.url;
+    const paramsStr = options.params ? JSON.stringify(options.params) : '_';
+
+    // url和参数都相同的请求认为是重复提交
+    if (urlArr[requestUrl] && urlArr[requestUrl][paramsStr]) {
+      return Promise.reject();
+    }
+
+    // 缓存请求的url和data
+    urlArr[requestUrl] = urlArr[requestUrl] || {};
+
+    const flag = 'req' + ++count;
+
+    urlArr[requestUrl][paramsStr] = flag;
+    options.flag = flag;
+  }
+
+  // 本地不存在token且是需要校验token的接口，直接提示去登录 （如首次访问首页的情况）
+  if (!isIgnoreToken && !accessToken) {
+    showFailToast('登录后才可以访问哦！');
+    removeUrlCache(options);
+    gotoLogin();
+    return Promise.reject();
+  }
+
+  request.interceptors.request.use(
     (config) => {
+      const loadingObj: ToastShowProps = {
+        content: '加载中...',
+        duration: 0 // 持续展示 toast
+      };
+
+      // 添加loading
+      if (options.isLoading) {
+        const loadingConfig = options.loadingConfig;
+
+        if (loadingConfig) {
+          Object.assign(loadingObj, loadingConfig);
+        }
+
+        Toast.show(loadingObj);
+      }
+
+      const methodsList = ['put', 'PUT', 'post', 'POST', 'patch', 'PATCH', 'delete', 'DELETE'];
+
+      if (config.method && methodsList.includes(config.method)) {
+        config['data'] = options.params;
+      } else {
+        config['params'] = options.params;
+      }
+
       return config;
     },
-    (err) => {
-      return err;
+    (error) => {
+      return Promise.reject(error);
     }
   );
 
-  axios.interceptors.response.use(
+  request.interceptors.response.use(
     (response) => {
       removeUrlCache(options);
       Toast.clear();
@@ -145,10 +189,7 @@ const http = (options: HttpOptions) => {
       removeUrlCache(options);
       Toast.clear();
 
-      if (options.isManualDealHttpError) {
-        // 手动处理http请求的错误
-        return error;
-      } else {
+      if (!options.isManualDealHttpError) {
         // 自动处理http请求的错误
         if (error.response) {
           switch (error.response.status) {
@@ -173,84 +214,18 @@ const http = (options: HttpOptions) => {
           showFailToast(error.message);
         }
       }
+
+      return Promise.reject(error);
     }
   );
 
-  return new Promise((resolve, reject) => {
-    // 前置拦截，进入axios之前进行登录态和请求是否能重复发送的拦截，不用放在interceptors.request中
+  const config: AxiosConfig = {
+    headers: headersObj,
+    url: options.url,
+    method: options.method || 'get'
+  };
 
-    // 本地不存在token且是需要校验token的接口，直接提示去登录 （如首次访问首页的情况）
-    if (!isIgnoreToken && !accessToken) {
-      showFailToast('登录后才可以访问哦！');
-      gotoLogin();
-      return;
-    }
-
-    // 请求不能重复
-    if (!options.isRepeatable) {
-      const requestUrl = options.url;
-      const paramsStr = options.params ? JSON.stringify(options.params) : '_';
-
-      // url和参数都相同的请求认为是重复提交
-      if (urlArr[requestUrl] && urlArr[requestUrl][paramsStr]) {
-        return;
-      }
-
-      // 缓存请求的url和data
-      urlArr[requestUrl] = urlArr[requestUrl] || {};
-
-      const flag = 'req' + ++count;
-
-      urlArr[requestUrl][paramsStr] = flag;
-      options.flag = flag;
-    }
-
-    const loadingObj: ToastShowProps = {
-      content: '加载中...',
-      duration: 0 // 持续展示 toast
-    };
-
-    // 添加loading
-    if (options.isLoading) {
-      const loadingConfig = options.loadingConfig;
-
-      if (loadingConfig) {
-        Object.assign(loadingObj, loadingConfig);
-      }
-
-      Toast.show(loadingObj);
-    }
-
-    interface Config {
-      headers: any;
-      url: string;
-      method: any;
-      data?: any;
-      params?: any;
-    }
-
-    const config: Config = {
-      headers: headersObj,
-      url: options.url,
-      method: options.method || 'get'
-    };
-
-    const methodsList = ['put', 'PUT', 'post', 'POST', 'patch', 'PATCH'];
-
-    if (methodsList.includes(config.method)) {
-      config['data'] = options.params;
-    } else {
-      config['params'] = options.params;
-    }
-
-    axios(config)
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+  return request(config);
 };
 
-export { http };
+export default http;
